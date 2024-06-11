fix derivative calculation in pemfc and hydrogen gas tank

src/fastga_he/models/propulsion/components/source/PEMFC/components/perf_fuel_consumption.py
@@ -54,10 +54,18 @@ class PerformancesPEMFCFuelConsumption(om.ExplicitComponent):
 
         self.declare_partials(
             of="*",
-            wrt="*",
+            wrt=["data:propulsion:he_power_train:pemfc_stack:" + pemfc_stack_id + ":effective_area","number_of_layers"],
             method="exact",
             rows=np.arange(number_of_points),
             cols=np.arange(number_of_points),
+        )
+
+        self.declare_partials(
+            of="*",
+            wrt="fc_current_density",
+            method="exact",
+            rows=np.arange(number_of_points),
+            cols=np.zeros(number_of_points),
         )
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):

src/fastga_he/models/propulsion/components/source/PEMFC/components/perf_pemfc_current_density.py
@@ -48,10 +48,18 @@ class PerformancesCurrentDensity(om.ExplicitComponent):
 
         self.declare_partials(
             of="*",
-            wrt="*",
+            wrt="data:propulsion:he_power_train:pemfc_stack:" + pemfc_stack_id + ":effective_area",
             method="exact",
             rows=np.arange(number_of_points),
             cols=np.arange(number_of_points),
+        )
+
+        self.declare_partials(
+            of="*",
+            wrt="dc_current_out",
+            method="exact",
+            rows=np.arange(number_of_points),
+            cols=np.zeros(number_of_points),
         )
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):

src/fastga_he/models/propulsion/components/source/PEMFC/components/perf_pemfc_power.py
@@ -27,7 +27,8 @@ class PerformancesPEMFCPower(om.ExplicitComponent):
 
         self.add_output("power_out", units="kW", val=np.full(number_of_points, 20.0))
 
-        self.declare_partials(of="*", wrt="*", method="exact")
+        self.declare_partials(of="*", wrt="*", method="exact",rows=np.arange(number_of_points),
+            cols=np.arange(number_of_points))
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
 
@@ -35,5 +36,5 @@ class PerformancesPEMFCPower(om.ExplicitComponent):
 
     def compute_partials(self, inputs, partials, discrete_inputs=None):
 
-        partials["power_out", "voltage_out"] = np.diag(inputs["dc_current_out"] / 1000.0)
-        partials["power_out", "dc_current_out"] = np.diag(inputs["voltage_out"] / 1000.0)
+        partials["power_out", "voltage_out"] = inputs["dc_current_out"] / 1000.0
+        partials["power_out", "dc_current_out"] = inputs["voltage_out"] / 1000.0

src/fastga_he/models/propulsion/components/source/PEMFC/components/perf_pemfc_voltage.py
@@ -50,10 +50,18 @@ class PerformancesPEMFCVoltage(om.ExplicitComponent):
 
         self.declare_partials(
             of=self.output_name,
-            wrt="*",
+            wrt="number_of_layers",
             method="exact",
             rows=np.arange(number_of_points),
             cols=np.arange(number_of_points),
+        )
+
+        self.declare_partials(
+            of=self.output_name,
+            wrt="single_layer_pemfc_voltage",
+            method="exact",
+            rows=np.arange(number_of_points),
+            cols=np.zeros(number_of_points),
         )
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):